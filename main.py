8cef43b0aaec79b9f3456573bf1d69a32d091da8
FedFedavi <kykypyk@gmail.com>
2024-07-16 20:08:01 +0400

    new code agr

---
+++ main.py
@@ -0,0 +1,16 @@
+class Teacher():
+    def teach(self):
+        print("Преподаватель учит")
+
+class School():
+    def __init__(self, new_teacher):
+        self.teacher = new_teacher
+
+    def start_lesson(self):
+        self.teacher.teach()
+
+my_teacher = Teacher()
+
+my_school = School(my_teacher)
+
+my_school.start_lesson()
