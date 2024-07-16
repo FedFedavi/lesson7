ddc98e479c4ac2b40d3fcf890bb962202429eb42
FedFedavi <kykypyk@gmail.com>
2024-07-16 20:41:26 +0400

    dz poly

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-class Teacher():
-    def teach(self):
-        print("Преподаватель учит")
+# Продемонстрировать принцип полиморфизма через реализацию разных классов,
+# представляющих геометрические формы, и метод для расчёта площади каждой формы.
+# # Создать базовый класс Shape с методом area(), который просто возвращает 0.
+# # Создать несколько производных классов для разных форм
+# (например, Circle, Rectangle, Square), каждый из которых переопределяет метод area().
+# # В каждом из этих классов метод area() должен возвращать площадь соответствующей фигуры.
+# Написать функцию, которая принимает объект класса Shape и выводит его площадь.
 
-class School():
-    def __init__(self, new_teacher):
-        self.teacher = new_teacher
+class Shape():
+    def area(self):
+        return 0
 
-    def start_lesson(self):
-        self.teacher.teach()
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
 
-my_teacher = Teacher()
+    def area(self):
+        return 3.14 * self.radius ** 2
 
-my_school = School(my_teacher)
 
-my_school.start_lesson()
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Square(Shape):
+    def __init__(self, width):
+        self.width = width
+
+    def area(self):
+        return self.width ** 2
+
+def print_area(shape):
+    print(f"Площадь - {shape.area()}")
+
+circle = Circle(5)
+square = Square(7)
+
+print_area(circle)
+print_area(square)
